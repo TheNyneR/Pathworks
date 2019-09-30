fe3604f20ff7bf8688620b2e9c8beccd71c5cc1b
Linus Probert <linus.probert@verisure.com>
2019-09-30 09:10:30 +0200

    Cleans up TestPositionUtil slightly

--- core/test/com/oliveshark/pathworks/framework/grid/TestPositionUtil.java
+++ core/test/com/oliveshark/pathworks/framework/grid/TestPositionUtil.java
@@ -8,22 +8,22 @@ import static com.oliveshark.pathworks.config.Config.*;
 import static com.oliveshark.pathworks.framework.grid.util.PositionUtil.getGridPositionFromScreenPosition;
 import static com.oliveshark.pathworks.framework.grid.util.PositionUtil.getPositionFromGridPosition;
 
-public class TestPositionUtil {
+class TestPositionUtil {
 
     @Test
-    public void testGetGridPositionFromScreenPosition() {
+    void testGetGridPositionFromScreenPosition() {
         Position beyondTopRight = getGridPositionFromScreenPosition(GRID_WIDTH*TILE_DIMENSION + 10, -100);
         Position beyondBottomLeft = getGridPositionFromScreenPosition(-100, GRID_HEIGHT*TILE_DIMENSION + 10);
         Position withinGrid = getGridPositionFromScreenPosition(320, 320);
 
-        Assertions.assertTrue(new Position(GRID_WIDTH - 1, GRID_HEIGHT - 1).equals(beyondTopRight));
-        Assertions.assertTrue(new Position(0, 0).equals(beyondBottomLeft));
-        Assertions.assertTrue(new Position(10, 13).equals(withinGrid));
+        Assertions.assertEquals(new Position<Integer>(GRID_WIDTH - 1, GRID_HEIGHT - 1), beyondTopRight);
+        Assertions.assertEquals(new Position<Integer>(0, 0), beyondBottomLeft);
+        Assertions.assertEquals(new Position<Integer>(10, 13), withinGrid);
     }
 
     @Test
-    public void testGetPositionFromGridPosition() {
+    void testGetPositionFromGridPosition() {
         Position pos = getPositionFromGridPosition(10, 10);
-        Assertions.assertTrue(new Position(320, 320).equals(pos));
+        Assertions.assertEquals(new Position<Integer>(320, 320), pos);
     }
 }
